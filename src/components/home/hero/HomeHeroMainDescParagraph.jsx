93088407a54707c82c9e01c3ce2243e29f46a9e0
Lovethrech <rachealdolapo45@gmail.com>
2025-09-06 22:29:13 +0100

    ...working on the home-page.

--- src/components/home/hero/HomeHeroMainDescParagraph.jsx
+++ src/components/home/hero/HomeHeroMainDescParagraph.jsx
@@ -1,7 +1,14 @@
 const HomeHeroMainDescParagraph=()=>{
     return(
-        <div className="home-hero">
-        
+        <div className="home-hero-main-desc-paragraph-main-ctn">
+            <p className="home-hero-main-desc-paragraph-main-ctn-name-desktop">
+                In March 2021, we set out on a journey to transform <br/>
+                healthcare in Africa, one solution at a time
+            </p>
+            <p className="home-hero-main-desc-paragraph-main-ctn-name-mobile">
+                remote - paperless - advanced <br/>
+                financial management
+            </p>
         </div>
     )
 };
