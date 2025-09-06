900899a0d36de4a45929ad14be949f9e6fa47a57
Lovethrech <rachealdolapo45@gmail.com>
2025-09-06 22:03:00 +0100

    ...working on the home-page.

--- src/components/home/hero/HomeHeroMainDescTitleText.jsx
+++ src/components/home/hero/HomeHeroMainDescTitleText.jsx
@@ -2,7 +2,7 @@ const HomeHeroMainDescTitleText=()=>{
     return(
         <div className="home-hero-main-desc-title-text-main-ctn">
             <h1 className="home-hero-main-desc-title-text-main-ctn-name">
-            
+                Your medical <br/> practice made easy
             </h1>
         </div>
     )
