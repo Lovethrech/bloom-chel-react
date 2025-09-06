4c214447fc7c04a765624a7342b203688671e097
Lovethrech <rachealdolapo45@gmail.com>
2025-09-06 20:27:00 +0100

    ...working on the home-page.

--- src/components/TryFreeBtn.jsx
+++ src/components/TryFreeBtn.jsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 const TryFreeBtn=()=>{
     return(
-        <div className="nav-header-main-ctn">
-            
+        <div className="try-for-free-btn-main-ctn">
+            <Link to="/">
+                <button className="try-for-free-btn">
+                
+                </button>
+            </Link>
         </div>
     )
 };
