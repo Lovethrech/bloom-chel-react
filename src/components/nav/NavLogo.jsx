2a052583b9233b483934991cc441a6e147fab36b
Lovethrech <rachealdolapo45@gmail.com>
2025-09-06 18:00:50 +0100

    ...startin the home page.

--- src/components/nav/NavLogo.jsx
+++ src/components/nav/NavLogo.jsx
@@ -1,9 +1,14 @@
-const NavHeader=()=>{
+import { Link } from "react-router-dom";
+
+const NavLogo=()=>{
     return(
-        <div className="nav-header">
-        
+        <div className="nav-logo-img-ctn">
+            <Link to="/">
+                <img src="@/assets/bloom.svg" alt="Bloom Desktop Logo" className="nav-logo-desktop-logo"/>
+                <img src="@/assets/bloom-M-colored.svg" alt="Bloom Mobile Logo" className="nav-logo-mobile-logo"/>
+            </Link>
         </div>
-    )
+    );
 };
 
-export default NavHeader;
+export default NavLogo;
