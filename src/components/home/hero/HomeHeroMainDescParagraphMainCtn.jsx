fe029654a55d40c88ccd7ec104bb7dd9202842d5
Lovethrech <rachealdolapo45@gmail.com>
2025-09-06 22:07:17 +0100

    ...working on the home-page.

--- src/components/home/hero/HomeHeroMainDescParagraphMainCtn.jsx
+++ src/components/home/hero/HomeHeroMainDescParagraphMainCtn.jsx
@@ -1,7 +1,11 @@
+import HomeHeroMainDescParagraph from './HomeHeroMainDescParagraph.jsx';
+import HomeHeroMainParagraphArrow from './HomeHeroMainParagraphArrow.jsx';
+
 const HomeHeroMainDescParagraphMainCtn=()=>{
     return(
         <div className="home-hero-main-desc-paragraph-main-ctn-main-ctn">
-        
+            <HomeHeroMainDescParagraph/>
+            <HomeHeroMainParagraphArrow/>
         </div>
     )
 };
