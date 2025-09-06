9b9ad0d913c9d54911e0131b849299c11acb1f61
Lovethrech <rachealdolapo45@gmail.com>
2025-09-06 17:28:55 +0100

    Initial commit.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
+import Home from "./components/home/Home";
 
 const App=()=>{
   return(
@@ -7,14 +8,11 @@ const App=()=>{
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/profile" element={<Profile />} />
+        // <Route path="/about" element={<About />} />
+        // <Route path="/contact" element={<Contact />} />
+        // <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
-    // <h2>
-    //   Functional Arrow Component
-    // </h2>
   )
 }
 
