5f79933bbe074467ed1c18604c5c7bb1d4f30184
Lovethrech <rachealdolapo45@gmail.com>
2025-09-06 18:29:01 +0100

    ...working on the home-page.

--- src/components/nav/NavHeader.jsx
+++ src/components/nav/NavHeader.jsx
@@ -1,5 +1,14 @@
+import { useState } from "react";
+import NavHeaderDetails from "src/data/nav-header-details.json";
 import NavLogo from "./NavLogo";
 import NavLink from "./NavLink";
+
+const navLinkColor=useState("#ffffff");
+const navLinkFontSize=useState("1.8vh");
+const tryFreeBtnBgColor=useState("#ffffff");
+const tryFreeBtnColor=useState("#A54dea");
+const displayNavMenuMobileListCtn=useState("none");
+const navMenuActiveIcon=useState("<i class='ri-menu-4-line'></i>");
 
 const NavHeader=()=>{
     return(
@@ -8,7 +17,7 @@ const NavHeader=()=>{
             <div className="nav-header-main-ctn-nav">
                 <NavLink
                     className="desktop-nav"
-                    
+
                 />
             </div>
         </div>
