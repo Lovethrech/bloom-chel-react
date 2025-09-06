31f036b053eff528fb5f5f3aff342ef5b1313e5c
Lovethrech <rachealdolapo45@gmail.com>
2025-09-06 18:26:31 +0100

    ...working on the home-page.

--- src/components/nav/NavHeader.jsx
+++ src/components/nav/NavHeader.jsx
@@ -1,11 +1,12 @@
 import NavLogo from "./NavLogo";
+import NavLink from "./NavLink";
 
 const NavHeader=()=>{
     return(
         <div className="nav-header-main-ctn">
             <NavLogo/>
             <div className="nav-header-main-ctn-nav">
-            
+                <NavLink/>
             </div>
         </div>
     )
