870730604462b28536becebdd130bbf5eb9ac1de
Lovethrech <rachealdolapo45@gmail.com>
2025-09-06 21:17:35 +0100

    ...working on the home-page.

--- src/components/nav/NavMenu.jsx
+++ src/components/nav/NavMenu.jsx
@@ -1,7 +1,7 @@
 const NavMenu=({navMenuActiveIcon})=>{
     return(
         <div className="nav-menu-img-ctn" dangerouslySetInnerHTML={{ __html: navMenuActiveIcon }}></div>
-    )
+    );
 };
 
 export default NavMenu;
