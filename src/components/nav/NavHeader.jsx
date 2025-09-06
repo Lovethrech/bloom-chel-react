b5453f4b10d9b0adc983f526d5efe2961f78bb37
Lovethrech <rachealdolapo45@gmail.com>
2025-09-06 18:25:01 +0100

    ...working on the home-page.

--- src/components/nav/NavHeader.jsx
+++ src/components/nav/NavHeader.jsx
@@ -4,6 +4,9 @@ const NavHeader=()=>{
     return(
         <div className="nav-header-main-ctn">
             <NavLogo/>
+            <div className="nav-header-main-ctn-nav">
+            
+            </div>
         </div>
     )
 };
