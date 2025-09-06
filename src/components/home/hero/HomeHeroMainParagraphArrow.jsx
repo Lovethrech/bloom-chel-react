f93cc44b1cda11b5f14f6a8b28f0d161098a528e
Lovethrech <rachealdolapo45@gmail.com>
2025-09-06 22:13:41 +0100

    ...working on the home-page.

--- src/components/home/hero/HomeHeroMainParagraphArrow.jsx
+++ src/components/home/hero/HomeHeroMainParagraphArrow.jsx
@@ -1,7 +1,13 @@
+const navToAboutComponent=()=>{
+    const target=document.getElementById('about');
+    if (!target) return;
+    target.scrollIntoView({ behavior: 'smooth' });
+}
+
 const HomeHeroMainParagraphArrow=()=>{
     return(
-        <div className="home-hero">
-        
+        <div className="home-hero-main-desc-paragraph-arrow-main-ctn" onClick={navToAboutComponent}>
+            <img src="src/assets/hero-arrow.svg" alt="Hero ArrowDown"/> 
         </div>
     )
 };
