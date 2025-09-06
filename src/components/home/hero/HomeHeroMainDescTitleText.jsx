d970e147b966ac2808754065e52397c41af3840e
Lovethrech <rachealdolapo45@gmail.com>
2025-09-06 22:00:52 +0100

    ...working on the home-page.

--- src/components/home/hero/HomeHeroMainDescTitleText.jsx
+++ src/components/home/hero/HomeHeroMainDescTitleText.jsx
@@ -1,7 +1,9 @@
 const HomeHeroMainDescTitleText=()=>{
     return(
-        <div className="home-hero">
-        
+        <div className="home-hero-main-desc-title-text-main-ctn">
+            <h1 className="home-hero-main-desc-title-text-main-ctn-name">
+            
+            </h1>
         </div>
     )
 };
