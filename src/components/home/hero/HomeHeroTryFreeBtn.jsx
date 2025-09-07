00a342b45dcad4f38aff04df1ec75d4427e68c62
Lovethrech <rachealdolapo45@gmail.com>
2025-09-07 06:58:38 +0100

    ...working on the home-page.

---
+++ src/components/home/hero/HomeHeroTryFreeBtn.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+const TryFreeBtn=({tryFreeBtnBgColor, tryFreeBtnColor})=>{
+    return(
+        <div className="try-for-free-btn-main-ctn">
+            <Link to="/">
+                <button 
+                    className="try-for-free-btn"
+                    style={{background:tryFreeBtnBgColor, color:tryFreeBtnColor}}
+                >
+                    Try for free
+                </button>
+            </Link>
+        </div>
+    )
+};
+
+export default TryFreeBtn;
