70bdd2d5a1dbcddadf25386faf1aba0832ff90df
Lovethrech <rachealdolapo45@gmail.com>
2025-09-08 15:14:51 +0100

    ...working on home component.

--- src/components/home/hero/HomeHeroMainDescTitleText.jsx
+++ src/components/home/hero/HomeHeroMainDescTitleText.jsx
@@ -6,7 +6,9 @@ const HomeHeroMainDescTitleText=()=>{
             <h1 
                 className="home-hero-main-desc-title-text-main-ctn-name"
             >
-                Your medical <br/> practice made easy
+                Your medical 
+                <br/> 
+                practice made easy
             </h1>
         </div>
     );
