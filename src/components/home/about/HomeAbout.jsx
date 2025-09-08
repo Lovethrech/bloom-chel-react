c4cafe29924c497adcb6f9ddc428c340d0baf0a6
Lovethrech <rachealdolapo45@gmail.com>
2025-09-08 17:05:34 +0100

    ...working on home component.

--- src/components/home/about/HomeAbout.jsx
+++ src/components/home/about/HomeAbout.jsx
@@ -1,11 +1,11 @@
-import './HomeAbout.module.css';
+import "./HomeAbout.css";
 
 const HomeAbout=()=>{
     return(
         <section id="about" className="home-about-main-ctn">
             <div className="home-about-mini-ctn">
                 <div className='home-about-mini-ctn-desktop-view'>
-
+                    lorem2000
                 </div>
             </div>
         </section>
