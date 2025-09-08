29e72bc8424c84f3c81dfc46b56382121f131092
Lovethrech <rachealdolapo45@gmail.com>
2025-09-08 17:08:34 +0100

    ...working on home component.

---
+++ src/components/home/about/HomeAboutDesc.jsx
@@ -0,0 +1,15 @@
+import "./HomeAboutDesc.css";
+
+const HomeAboutDesc=()=>{
+    return(
+        <section id="about" className="home-about-main-ctn">
+            <div className="home-about-mini-ctn">
+                <div className='home-about-mini-ctn-desktop-view'>
+                    
+                </div>
+            </div>
+        </section>
+    )
+};
+
+export default HomeAboutDesc;
