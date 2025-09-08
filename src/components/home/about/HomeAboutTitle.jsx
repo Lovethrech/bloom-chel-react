ed63a86d55d0faaa7871e42205c5afd305debe8e
Lovethrech <rachealdolapo45@gmail.com>
2025-09-08 17:14:25 +0100

    ...working on home component.

---
+++ src/components/home/about/HomeAboutTitle.jsx
@@ -0,0 +1,14 @@
+import "./HomeAboutTitle.css";
+
+const HomeAboutTitle=()=>{
+    return(
+        <div className="home-about-title-main-ctn">
+            <h2 class="home-about-title-main-ctn-name">
+                The <b class="home-about-title-main-ctn-name-colored">bloom</b><br/>
+                way
+            </h2>
+        </div>
+    )
+};
+
+export default HomeAboutTitle;
