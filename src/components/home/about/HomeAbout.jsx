769c41047333404cc0ca740fdc056620944be37d
Lovethrech <rachealdolapo45@gmail.com>
2025-09-08 16:38:40 +0100

    ...working on home component.

--- src/components/home/about/HomeAbout.jsx
+++ src/components/home/about/HomeAbout.jsx
@@ -3,7 +3,9 @@ import '../Home.css';
 const HomeAbout=()=>{
     return(
         <section id="about" className="home-about-main-ctn">
+            <div className="home-about-mini-ctn">
             
+            </div>
         </section>
     )
 };
