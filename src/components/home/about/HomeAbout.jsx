24fb301be163f244272b165b616ce6090e4beb5a
Lovethrech <rachealdolapo45@gmail.com>
2025-09-08 16:39:23 +0100

    ...working on home component.

--- src/components/home/about/HomeAbout.jsx
+++ src/components/home/about/HomeAbout.jsx
@@ -4,7 +4,9 @@ const HomeAbout=()=>{
     return(
         <section id="about" className="home-about-main-ctn">
             <div className="home-about-mini-ctn">
-            
+                <div className='home-about-mini-ctn-desktop-view'>
+                    
+                </div>
             </div>
         </section>
     )
