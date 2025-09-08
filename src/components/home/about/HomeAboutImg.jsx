2b07796d57d6b4c9dca7df8f7e944a7a0f4d7253
Lovethrech <rachealdolapo45@gmail.com>
2025-09-08 17:10:55 +0100

    ...working on home component.

---
+++ src/components/home/about/HomeAboutImg.jsx
@@ -0,0 +1,15 @@
+import "./HomeAboutImg.css";
+
+const HomeAboutImg=()=>{
+    return(
+        <section id="about" className="home-about-main-ctn">
+            <div className="home-about-mini-ctn">
+                <div className='home-about-mini-ctn-desktop-view'>
+                    
+                </div>
+            </div>
+        </section>
+    )
+};
+
+export default HomeAboutImg;
