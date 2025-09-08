b88097820b8ceb31ea2a1af19216ef3e9f089f6f
Lovethrech <rachealdolapo45@gmail.com>
2025-09-08 14:06:34 +0100

    ...working on home component.

--- src/components/home/hero/HomeHeroMainDescParagraph.jsx
+++ src/components/home/hero/HomeHeroMainDescParagraph.jsx
@@ -3,11 +3,11 @@ import '../Home.css';
 const HomeHeroMainDescParagraph=()=>{
     return(
         <div className="home-hero-main-desc-paragraph-main-ctn">
-            <p className="home-hero-main-desc-paragraph-main-ctn-name-desktop">
+            <p id="home-hero-main-desc-paragraph-main-ctn-name-desktop">
                 In March 2021, we set out on a journey to transform <br/>
                 healthcare in Africa, one solution at a time
             </p>
-            <p className="home-hero-main-desc-paragraph-main-ctn-name-mobile">
+            <p id="home-hero-main-desc-paragraph-main-ctn-name-mobile">
                 remote - paperless - advanced <br/>
                 financial management
             </p>
