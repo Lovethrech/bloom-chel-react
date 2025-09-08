f72251d90646ca7c1de320c4f132b638e8157b47
Lovethrech <rachealdolapo45@gmail.com>
2025-09-08 15:06:59 +0100

    ...working on home component.

--- src/components/nav/NavLink.jsx
+++ src/components/nav/NavLink.jsx
@@ -3,8 +3,13 @@ import './Nav.css';
 
 const NavLink=({name, link, color, fontSize})=>{
     return(
-        <div className="nav-link-main-ctn">
-            <Link to={link} className="link">
+        <div 
+            className="nav-link-main-ctn"
+        >
+            <Link 
+                to={link} 
+                className="link"
+            >
                 <p className="nav-link-main-ctn-desc" style={{color:color, fontSize:fontSize}}>
                     {name}
                 </p>
