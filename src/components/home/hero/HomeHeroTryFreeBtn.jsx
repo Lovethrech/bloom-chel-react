98413c3d3edd282d2c962f35557f47f83f5fe7cb
Lovethrech <rachealdolapo45@gmail.com>
2025-09-08 15:16:06 +0100

    ...working on home component.

--- src/components/home/hero/HomeHeroTryFreeBtn.jsx
+++ src/components/home/hero/HomeHeroTryFreeBtn.jsx
@@ -2,8 +2,13 @@ import { Link } from "react-router-dom";
 
 const HomeHeroTryFreeBtn=({tryFreeBtnBgColor,tryFreeBtnColor})=>{
     return(
-        <div className="try-for-free-btn-main-ctn" id="home-hero-try-for-free-btn-main-ctn">
-            <Link to="/">
+        <div 
+            className="try-for-free-btn-main-ctn" 
+            id="home-hero-try-for-free-btn-main-ctn"
+        >
+            <Link 
+                to="/"
+            >
                 <button 
                     className="try-for-free-btn"
                     style={{background:tryFreeBtnBgColor, color:tryFreeBtnColor}}
