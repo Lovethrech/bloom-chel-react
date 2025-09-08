d2275a01f6827b573914fdd816003dbd15d8a508
Lovethrech <rachealdolapo45@gmail.com>
2025-09-08 17:06:32 +0100

    ...working on home component.

--- src/components/home/about/HomeAbout.jsx
+++ src/components/home/about/HomeAbout.jsx
@@ -5,7 +5,7 @@ const HomeAbout=()=>{
         <section id="about" className="home-about-main-ctn">
             <div className="home-about-mini-ctn">
                 <div className='home-about-mini-ctn-desktop-view'>
-                    lorem2000
+                    
                 </div>
             </div>
         </section>
