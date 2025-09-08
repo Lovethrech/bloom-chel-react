cb06d21084c12bb74ed62fdd74670a9664d71c5a
Lovethrech <rachealdolapo45@gmail.com>
2025-09-08 15:12:20 +0100

    ...working on home component.

--- src/components/nav/NavTryFreeBtn.jsx
+++ src/components/nav/NavTryFreeBtn.jsx
@@ -2,8 +2,14 @@ import { Link } from "react-router-dom";
 
 const NavTryFreeBtn=({tryFreeBtnBgColor,tryFreeBtnColor})=>{
     return(
-        <div className="try-for-free-btn-main-ctn" id="nav-try-for-free-btn-main-ctn">
-            <Link to="/" className="link">
+        <div 
+            className="try-for-free-btn-main-ctn" 
+            id="nav-try-for-free-btn-main-ctn"
+        >
+            <Link 
+                to="/" 
+                className="link"
+            >
                 <button 
                     className="try-for-free-btn"
                     style={{background:tryFreeBtnBgColor, color:tryFreeBtnColor}}
@@ -12,7 +18,7 @@ const NavTryFreeBtn=({tryFreeBtnBgColor,tryFreeBtnColor})=>{
                 </button>
             </Link>
         </div>
-    )
+    );
 };
 
 export default NavTryFreeBtn;
