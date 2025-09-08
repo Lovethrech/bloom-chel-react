83ae3acd4a5d95513dbfb18c8100c07026f26543
Lovethrech <rachealdolapo45@gmail.com>
2025-09-08 14:26:02 +0100

    ...working on home component.

--- src/components/nav/NavLogo.jsx
+++ src/components/nav/NavLogo.jsx
@@ -4,8 +4,8 @@ const NavLogo=()=>{
     return(
         <div className="nav-logo-img-ctn">
             <Link to="/">
-                <img src="/bloom.svg" alt="Bloom Desktop Logo" className="nav-logo-desktop-logo"/>
-                <img src="src/assets/bloom-M-colored.svg" alt="Bloom Mobile Logo" className="nav-logo-mobile-logo"/>
+                <img src="/bloom.svg" alt="Bloom Desktop Logo" id="nav-logo-desktop-logo"/>
+                <img src="src/assets/bloom-M-colored.svg" alt="Bloom Mobile Logo" id="nav-logo-mobile-logo"/>
             </Link>
         </div>
     );
