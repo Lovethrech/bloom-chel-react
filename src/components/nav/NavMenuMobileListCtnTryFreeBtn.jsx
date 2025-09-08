af68b4c7aeda762db2b34158d093742fd5381144
Lovethrech <rachealdolapo45@gmail.com>
2025-09-08 14:39:57 +0100

    ...working on home component.

---
+++ src/components/nav/NavMenuMobileListCtnTryFreeBtn.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+const NavTryFreeBtn=({tryFreeBtnBgColor,tryFreeBtnColor})=>{
+    return(
+        <div className="try-for-free-btn-main-ctn" id="nav-try-for-free-btn-main-ctn">
+            <Link to="/" className="link">
+                <button 
+                    className="try-for-free-btn"
+                    style={{background:tryFreeBtnBgColor, color:tryFreeBtnColor}}
+                >
+                    Try for free
+                </button>
+            </Link>
+        </div>
+    )
+};
+
+export default NavTryFreeBtn;
