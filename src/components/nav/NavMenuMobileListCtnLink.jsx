17c5b388327627fdb633f8542937a738321e89ce
Lovethrech <rachealdolapo45@gmail.com>
2025-09-08 14:51:48 +0100

    ...working on home component.

---
+++ src/components/nav/NavMenuMobileListCtnLink.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import './Nav.css';
+
+const NavLink=({name, link, color, fontSize})=>{
+    return(
+        <div className="nav-link-main-ctn" id="nav-link-main-ctn">
+            <Link to={link} className="link">
+                <p className="nav-link-main-ctn-desc" style={{color:color, fontSize:fontSize}}>
+                    {name}
+                </p>
+            </Link>
+        </div>
+    )
+};
+
+export default NavLink;
