3238fe34efa3def207ad6fdbd36ab6cbac61bc9d
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 15:33:16 +0100

    ...working on home component.

---
+++ src/components/home/blog/HomeBlog.jsx
@@ -0,0 +1,12 @@
+
+const HomeBlog=()=>{
+    return(
+        <section className="home-about-main-ctn">
+            <div className="home-about-mini-ctn">
+                
+            </div>
+        </section>
+    )
+};
+
+export default HomeBlog;
