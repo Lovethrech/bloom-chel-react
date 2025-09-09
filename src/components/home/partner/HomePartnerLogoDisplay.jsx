0170c2dd23cec86f25030abb658c06f3c3d9143c
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 19:12:00 +0100

    ...working on home component.

--- src/components/home/partner/HomePartnerLogoDisplay.jsx
+++ src/components/home/partner/HomePartnerLogoDisplay.jsx
@@ -1,8 +1,8 @@
 import "./HomePartnerLogoDisplay.css";
 const HomePartnerLogoDisplay=()=>{
     return(
-        <div className="home-about-main-ctn">
-            <div className="home-about-mini-ctn">
+        <div className="home-partner-logo-display-main-ctn">
+            <div className="home-partner-logo-display-mini-ctn">
                 
             </div>
         </div>
