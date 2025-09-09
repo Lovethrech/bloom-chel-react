5fd29a534ebb1a40fee467b33e865314687cb0ef
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 15:31:09 +0100

    ...working on home component.

---
+++ src/components/home/achieved/HomeArchieved.jsx
@@ -0,0 +1,12 @@
+
+const HomeArchieved=()=>{
+    return(
+        <section id="contact" className="home-about-main-ctn">
+            <div className="home-about-mini-ctn">
+                
+            </div>
+        </section>
+    )
+};
+
+export default HomeArchieved;
