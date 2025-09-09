dc7f3de9d453f472dca7acb88132e56844bc9896
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 21:33:20 +0100

    ...working on home component.

--- src/components/pricing/Pricing.jsx
+++ src/components/pricing/Pricing.jsx
@@ -1,12 +1,15 @@
 
 import "./Pricing.css";
+import PricingHero from './hero/PricingHero';
+import PricingPackage from './package/PricingPackage';
 import Footer from "../footer/Footer";
 
 
 const Pricing=()=>{
     return(
         <main>
-            
+            <PricingHero/>
+            <PricingPackage/>
             <Footer/>
         </main>
     )
