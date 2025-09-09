4e7a9fce2b4cfb98af48af2c2cf2e0a75c58741a
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 17:54:45 +0100

    ...working on home component.

--- src/components/home/blog/HomeBlog.jsx
+++ src/components/home/blog/HomeBlog.jsx
@@ -1,9 +1,18 @@
 import "./HomeBlog.css";
+import HomeBlogBoxMainCtn from "./HomeBlogBoxMainCtn";
+import HomeBlogDesc from "./HomeBlogDesc";
+import HomeBlogTitle from "./HomeBlogTitle";
+import HomeBlogVisitBtn from "./HomeBlogVisitBtn";
 const HomeBlog=()=>{
     return(
         <section className="home-blog-main-ctn">
             <div className="home-blog-mini-ctn">
-                
+                <HomeBlogTitle/>
+                <HomeBlogDesc/>
+                <br/>
+                <HomeBlogBoxMainCtn/>
+                <br/>
+                <HomeBlogVisitBtn/>
             </div>
         </section>
     )
