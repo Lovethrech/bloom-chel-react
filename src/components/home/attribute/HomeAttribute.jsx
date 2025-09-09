3e0414c6fa46300acdfe812d07ddfb4422bd0ab1
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 16:05:55 +0100

    ...working on home component.

--- src/components/home/attribute/HomeAttribute.jsx
+++ src/components/home/attribute/HomeAttribute.jsx
@@ -1,4 +1,25 @@
 import "./HomeAttribute.css";
+
+const HomeAttributeDetails=[
+    {
+        "id":"1",
+        "name": "strategic planning",
+        "imgSrc":"<i class='bx  bx-calendar-check'  ></i> ",
+        "description":"Transform your business with data driven decision making. Get real-time insight on sales, growth, expenses and a lot more."
+    },
+    {
+        "id":"2",
+        "name": "remote & paperless",
+        "imgSrc":"<i class='bx  bx-file-report'  ></i> ",
+        "description":"Run your business from anywhere you are in the world. Get real time updates on what's happening and take your business paperless."
+    },
+    {
+        "id":"3",
+        "name": "manage finances",
+        "imgSrc":"<i class='bx  bx-chart-trend'  ></i> ",
+        "description":"Track all your HMO payments, ​vendor payments, imprest, as well ​as other income and expenditure ​accounts."
+    }
+]
 const HomeAttribute=()=>{
     return(
         <section className="home-attribute-main-ctn">
