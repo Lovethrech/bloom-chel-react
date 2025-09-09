c356d78c9ab2e1c4afb9c075046cbc10303ccc88
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 19:22:23 +0100

    ...working on home component.

--- src/components/home/partner/HomePartnerDesc.jsx
+++ src/components/home/partner/HomePartnerDesc.jsx
@@ -3,7 +3,7 @@ const HomePartnerDesc=()=>{
     return(
         <div className="home-partner-desc-main-ctn">
             <p className="home-partner-desc-main-ctn-name">
-                Take a loot at our strategic partners who 
+                Take a look at our strategic partners who 
                 we work with round the clock to provide 
                 satisfactory services.
             </p>
