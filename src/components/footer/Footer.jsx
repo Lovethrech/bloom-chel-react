08d87f55a60d503cdf1112f34a15116ce88e80d0
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 19:31:35 +0100

    ...working on home component.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,34 +3,9 @@ import "./Footer.css";
 const NavHeader=()=>{
 
     return(
-        <header className="nav-header-main-ctn">
-            <NavLogo/>
-            <div className="nav-header-main-ctn-nav">
-                {NavHeaderDetails.map(NavHeaderDetail=>{
-                    return(
-                        <NavLink
-                            key={NavHeaderDetail.id}
-                            name={NavHeaderDetail.name}
-                            link={NavHeaderDetail.link}
-                            color={navLinkColor}
-                            fontSize={navLinkFontSize}
-                        />
-                    )
-                })}
-                <NavTryFreeBtn
-                    tryFreeBtnBgColor={tryFreeBtnBgColor}
-                    tryFreeBtnColor={tryFreeBtnColor}
-                />
-                <NavMenu
-                    navMenuActiveIcon={navMenuActiveIcon}
-                    setNavMenuActiveIcon={setNavMenuActiveIcon}
-                    setDisplayNavMenuMobileListCtn={setDisplayNavMenuMobileListCtn}
-                />
-                <NavMenuMobileListCtn
-                    displayNavMenuMobileListCtn={displayNavMenuMobileListCtn}
-                />
-            </div>
-        </header>
+        <footer className="footer">
+            
+        </footer>
     )
 };
 
