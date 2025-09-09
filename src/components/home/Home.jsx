b8143da881b4201ee666a566781325a5c2091b11
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 15:37:09 +0100

    ...working on home component.

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,12 +2,16 @@ import HomeHero from "./hero/HomeHero";
 import HomeAbout from "./about/HomeAbout";
 import HomeContact from "./contact/HomeContact";
 import "./Home.css";
+import HomeAttribute from "./attribute/HomeAttribute";
+import HomeAchieved from "./achieved/HomeAchieved";
 const Home=()=>{
     return(
         <main>
             <HomeHero/>
             <HomeAbout/>
             <HomeContact/>
+            <HomeAttribute/>
+            <HomeAchieved/>
         </main>
     )
 };
