d7bad7ad1e16fea8b446963e20ed27ab2fb9d620
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 21:03:45 +0100

    ...working on home component.

--- src/components/footer/FooterLinkIconCtn.jsx
+++ src/components/footer/FooterLinkIconCtn.jsx
@@ -1,11 +1,36 @@
 import "./FooterLinkIconCtn.css";
+const FooterLinkIcons=[
+    {
+        "id":1,
+        "name":"<i class='bx bxl-instagram'></i>"
+    },
+    {
+        "id":2,
+        "name":"<i class='bx bxl-twitter' ></i>"
+    },
+    {
+        "id":3,
+        "name":"<i class='bx bxl-linkedin-square'></i>"
+    },
+    {
+        "id":4,
+        "name":"<i class='bx bxl-whatsapp' ></i>"
+    }
+];
 
 const FooterLinkIconCtn=()=>{
 
     return(
         <div className="footer-link-icon-ctn-main-ctn">
             <div className="footer-link-icon-ctn-mini-ctn">
-            
+                {FooterLinkIcons.map(FooterLinkIcon=>{
+                    return(
+                        <NavLink
+                            key={FooterLinkIcon.id}
+                            name={FooterLinkIcon.name}
+                        />
+                    )
+                })}
             </div>
         </div>
     )
