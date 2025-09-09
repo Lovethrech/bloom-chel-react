56f02e0f457a2e3635055c4306dd6027543cfeaa
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 21:18:43 +0100

    ...working on home component.

---
+++ src/components/pricing/Pricing.jsx
@@ -0,0 +1,15 @@
+
+import "./Pricing.css";
+import Footer from "../footer/Footer";
+
+
+const Home=()=>{
+    return(
+        <main>
+            
+            <Footer/>
+        </main>
+    )
+};
+
+export default Home;
