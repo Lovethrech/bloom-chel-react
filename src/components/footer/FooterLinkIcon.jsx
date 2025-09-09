563874f34bff1e1ea37fec67543ada26bfddb693
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 21:08:34 +0100

    ...working on home component.

--- src/components/footer/FooterLinkIcon.jsx
+++ src/components/footer/FooterLinkIcon.jsx
@@ -3,7 +3,10 @@ import "./FooterLinkIcon.css";
 const FooterLinkIcon=({name})=>{
 
     return(
-        <div className="footer-link-icon-main-ctn" dangerouslySetInnerHTML={{ __html: name }}>
+        <div 
+            className="footer-link-icon-main-ctn" 
+            dangerouslySetInnerHTML={{ __html: name }}
+        >
         </div>
     )
 };
