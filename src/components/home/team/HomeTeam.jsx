e1e660437472e196bf882fa6503916e166fd6b55
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 15:35:29 +0100

    ...working on home component.

---
+++ src/components/home/team/HomeTeam.jsx
@@ -0,0 +1,12 @@
+
+const HomeTeam=()=>{
+    return(
+        <section className="home-about-main-ctn">
+            <div className="home-about-mini-ctn">
+                
+            </div>
+        </section>
+    )
+};
+
+export default HomeTeam;
