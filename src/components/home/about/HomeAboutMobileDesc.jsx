3b72f6f78875834676c00e949bf3e1d35820ad96
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 14:49:39 +0100

    ...working on home component.

---
+++ src/components/home/about/HomeAboutMobileDesc.jsx
@@ -0,0 +1,16 @@
+import "./HomeAboutMobileDesc.css";
+
+const HomeAboutMobileDesc=()=>{
+    return(
+        <div className="home-about-desc-main-ctn">
+            <p className="home-about-desc-main-ctn-name">
+                Access all important documents in one place, Run 
+                your business from anywhere in the world, Manage 
+                all income and expenditure in one place, Connect 
+                with the broader medical community
+            </p>
+        </div>
+    )
+};
+
+export default HomeAboutMobileDesc;
