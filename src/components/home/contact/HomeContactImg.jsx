fb05beaa091ff0810b237c99ed51623fa497ff9b
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 15:55:20 +0100

    ...working on home component.

--- src/components/home/contact/HomeContactImg.jsx
+++ src/components/home/contact/HomeContactImg.jsx
@@ -2,9 +2,9 @@ import "./HomeContactImg.css";
 const HomeContactImg=()=>{
     return(
         <div className="home-contact-img-main-ctn">
-            <img src="src/assets/home-contact-img.svg" alt="Home Contact Image">
+            <img src="src/assets/home-contact-img.svg" alt="Home Contact Image"/>
         </div>
-    )
+    );
 };
 
 export default HomeContactImg;
