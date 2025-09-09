b254ea4295e71f0e02b8ad6c429735bf15cd7cd7
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 20:52:45 +0100

    ...working on home component.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,21 @@
 import "./Footer.css";
+import FooterDesc from "./FooterDesc";
+import FooterLinkIconCtn from "./FooterLinkIconCtn";
+import FooterLinkMail from "./FooterLinkMail";
+import FooterLogo from "./FooterLogo";
+import FooterRight from "./FooterRight";
 
 const Footer=()=>{
 
     return(
         <footer className="footer-main-ctn">
             <div className="footer-mini-ctn">
-            
+                <FooterLogo/>
+                <FooterDesc/>
+                <br/>
+                <FooterLinkMail/>
+                <FooterLinkIconCtn/>
+                <FooterRight/>
             </div>
         </footer>
     )
