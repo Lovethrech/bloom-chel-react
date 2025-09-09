a19a68414b33d34c1d6a83b8b807d6f9c3cc900e
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 15:34:08 +0100

    ...working on home component.

---
+++ src/components/home/customer/HomeCustomer.jsx
@@ -0,0 +1,12 @@
+
+const HomeCustomer=()=>{
+    return(
+        <section className="home-about-main-ctn">
+            <div className="home-about-mini-ctn">
+                
+            </div>
+        </section>
+    )
+};
+
+export default HomeCustomer;
