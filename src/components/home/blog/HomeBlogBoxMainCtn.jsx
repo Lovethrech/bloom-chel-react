50249b5567713bc69dd6b04c13c4135385c8ae56
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 18:08:02 +0100

    ...working on home component.

--- src/components/home/blog/HomeBlogBoxMainCtn.jsx
+++ src/components/home/blog/HomeBlogBoxMainCtn.jsx
@@ -1,12 +1,14 @@
-import HomeBlogBoxOne from 'src/components/home/blog/HomeBlogBoxOne.css';
-import HomeBlogBoxTwo from 'src/components/home/blog/HomeBlogBoxTwo.css';
-import HomeBlogBoxThree from 'src/components/home/blog/HomeBlogBoxThree.css';
+import HomeBlogBoxOne from './HomeBlogBoxOne.css';
+import HomeBlogBoxTwo from './HomeBlogBoxTwo.css';
+import HomeBlogBoxThree from './HomeBlogBoxThree.css';
 import "./HomeBlogBoxMainCtn.css";
 const HomeBlogBoxMainCtn=()=>{
     return(
         <div className="home-bog-box-main-ctn-main-ctn">
             <div className="home-bog-box-main-ctn-mini-ctn">
                 <HomeBlogBoxOne/>
+                <HomeBlogBoxTwo/>
+                <HomeBlogBoxThree/>
             </div>
         </div>
     )
