356cea0b4980f6e03b15252ac0d6ce96ee86acc2
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 15:32:41 +0100

    ...working on home component.

--- src/components/home/attribute/HomeAttribute.jsx
+++ src/components/home/attribute/HomeAttribute.jsx
@@ -1,7 +1,7 @@
 
 const HomeAttribute=()=>{
     return(
-        <section id="contact" className="home-about-main-ctn">
+        <section className="home-about-main-ctn">
             <div className="home-about-mini-ctn">
                 
             </div>
