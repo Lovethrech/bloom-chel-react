124d214880a018f6b346fa0a96cf958facc07e34
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 21:06:06 +0100

    ...working on home component.

--- src/components/footer/FooterLinkIconCtn.jsx
+++ src/components/footer/FooterLinkIconCtn.jsx
@@ -20,7 +20,6 @@ const FooterLinkIcons=[
 ];
 
 const FooterLinkIconCtn=()=>{
-
     return(
         <div className="footer-link-icon-ctn-main-ctn">
             <div className="footer-link-icon-ctn-mini-ctn">
