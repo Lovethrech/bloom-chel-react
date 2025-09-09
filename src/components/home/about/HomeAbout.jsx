4178268dcffd69f25e7c23b25888236bb16d503b
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 14:31:47 +0100

    ...working on home component.

--- src/components/home/about/HomeAbout.jsx
+++ src/components/home/about/HomeAbout.jsx
@@ -5,6 +5,9 @@ import HomeAboutDesktopTryFreeBtn from "./HomeAboutDesktopTryFreeBtn";
 // <HomeAboutTitle id="home-about-title"/>
 //                 <HomeAboutDesc className="home-about-desc"/>
 import "./HomeAbout.css";
+
+const tryFreeBtnBgColor="linear-gradient(to bottom, #A54dea, #594edc)";
+const tryFreeBtnColor="#ffffff";
 const HomeAbout=()=>{
     return(
         <section id="about" className="home-about-main-ctn">
@@ -12,7 +15,10 @@ const HomeAbout=()=>{
                 <div className='home-about-mini-ctn-desktop-view'>
                     <HomeAboutTitle/>
                     <HomeAboutDesc/>
-                    <HomeAboutDesktopTryFreeBtn/>
+                    <HomeAboutDesktopTryFreeBtn
+                        tryFreeBtnBgColor={tryFreeBtnBgColor}
+                        tryFreeBtnColor={tryFreeBtnColor}
+                    />
                     <br/>
                 </div>
                 
