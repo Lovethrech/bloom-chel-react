28dcff480e6348d262ffc194a938a48af0555bcf
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 16:28:54 +0100

    ...working on home component.

--- src/components/home/achieved/HomeAchievedDescTop.jsx
+++ src/components/home/achieved/HomeAchievedDescTop.jsx
@@ -1,10 +1,12 @@
 import "./HomeAchievedDescTop.css";
 const HomeAchievedDescTop=()=>{
     return(
-        <div className="home-achieved-main-ctn">
-            <div className="home-achieved-mini-ctn">
-                
-            </div>
+        <div className="home-achieved-desc-top-main-ctn">
+            <p className="home-achieved-desc-top-main-ctn-name">
+                What we have achieved
+                <br/>
+                in the past two years
+            </p>
         </div>
     )
 };
