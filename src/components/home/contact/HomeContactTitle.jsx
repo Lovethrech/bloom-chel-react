6e0ffb66ae4af1ea7fc54ecff5d5590a6e3f9ccc
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 15:45:52 +0100

    ...working on home component.

---
+++ src/components/home/contact/HomeContactTitle.jsx
@@ -0,0 +1,12 @@
+import "./HomeContactTitle.css";
+const HomeContactTitle=()=>{
+    return(
+        <section className="home-contact-main-ctn">
+            <div className="home-contact-mini-ctn">
+                
+            </div>
+        </section>
+    )
+};
+
+export default HomeContactTitle;
