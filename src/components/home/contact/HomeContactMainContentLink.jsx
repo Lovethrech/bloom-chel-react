f42c7d289dd7f955f07ef48ee382eb73f67f4c70
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 15:58:20 +0100

    ...working on home component.

--- src/components/home/contact/HomeContactMainContentLink.jsx
+++ src/components/home/contact/HomeContactMainContentLink.jsx
@@ -4,7 +4,12 @@ const HomeContactMainContentLink=()=>{
     return(
         <div className="hhome-contact-main-content-link-main-ctn">
             <Link to="" className="link">
-            
+                <div className="home-contact-main-content-link-mini-ctn">
+                    <p class="home-contact-main-content-link-mini-ctn-name">
+                        contact us
+                    </p>
+                    <i class='bx  bx-arrow-up-right-stroke'  ></i> 
+                </div>
             </Link>
         </div>
     )
