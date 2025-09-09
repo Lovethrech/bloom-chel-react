d30f2e5427b8d89a766b5bc3011e14e020facb1d
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 15:46:45 +0100

    ...working on home component.

--- src/components/home/contact/HomeContact.jsx
+++ src/components/home/contact/HomeContact.jsx
@@ -1,9 +1,13 @@
 import "./HomeContact.css";
+import HomeContactMainContent from "./HomeContactMainContent";
+import HomeContactTitle from "./HomeContactTitle";
 const HomeContact=()=>{
     return(
         <section className="home-contact-main-ctn">
             <div className="home-contact-mini-ctn">
-                
+                <HomeContactTitle/>
+                <br/>
+                <HomeContactMainContent/>
             </div>
         </section>
     )
