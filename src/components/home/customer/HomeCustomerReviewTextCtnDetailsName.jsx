56c276d021c034c016e081d180e38944692e9b55
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 17:21:13 +0100

    ...working on home component.

---
+++ src/components/home/customer/HomeCustomerReviewTextCtnDetailsName.jsx
@@ -0,0 +1,12 @@
+import "./HomeCustomerReviewTextCtnDetailsName.css";
+const HomeCustomerReviewTextCtnDetailsName=()=>{
+    return(
+        <div className="home-about-main-ctn">
+            <div className="home-about-mini-ctn">
+                
+            </div>
+        </div>
+    )
+};
+
+export default HomeCustomerReviewTextCtnDetailsName;
---
+++ src/components/home/customer/HomeCustomerReviewTextCtnDetailsYear.jsx
@@ -0,0 +1,12 @@
+import "./HomeCustomerReviewTextCtnDetailsYear.css";
+const HomeCustomerReviewTextCtnDetailsYear=()=>{
+    return(
+        <div className="home-about-main-ctn">
+            <div className="home-about-mini-ctn">
+                
+            </div>
+        </div>
+    )
+};
+
+export default HomeCustomerReviewTextCtnDetailsYear;
