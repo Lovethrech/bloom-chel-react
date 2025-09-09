392084e5a54f63de10de867f2c53172c8248b6d2
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 21:46:38 +0100

    ...working on home component.

---
+++ src/components/pricing/package/PricingPackage.jsx
@@ -0,0 +1,13 @@
+import './PricingPackage.css';
+
+const PricingPackage=()=>{
+    return(
+        <div 
+            className="nav-link-main-ctn"
+        >
+            
+        </div>
+    );
+};
+
+export default PricingPackage;
