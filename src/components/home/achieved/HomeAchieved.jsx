049eb2881a516d340662e95f2e5ef7743a193866
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 16:27:18 +0100

    ...working on home component.

--- src/components/home/achieved/HomeAchieved.jsx
+++ src/components/home/achieved/HomeAchieved.jsx
@@ -1,9 +1,12 @@
 import "./HomeAchieved.css";
+import HomeAchievedDescBottom from "./HomeAchievedDescBottom";
+import HomeAchievedDescTop from "./HomeAchievedDescTop";
 const HomeAchieved=()=>{
     return(
         <section className="home-achieved-main-ctn">
             <div className="home-achieved-mini-ctn">
-                
+                <HomeAchievedDescTop/>
+                <HomeAchievedDescBottom/>
             </div>
         </section>
     )
