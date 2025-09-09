c771fdaac552ed627a29bb69747d95c9ca56bbad
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 18:53:48 +0100

    ...working on home component.

--- src/components/home/team/HomeTeam.jsx
+++ src/components/home/team/HomeTeam.jsx
@@ -1,9 +1,19 @@
 import "./HomeTeam.css";
+import HomeTeamBoxOne from "./HomeTeamBoxOne";
+import HomeTeamBoxTwo from "./HomeTeamBoxTwo";
+import HomeTeamDesc from "./HomeTeamDesc";
+import HomeTeamTitle from "./HomeTeamTitle";
 const HomeTeam=()=>{
     return(
-        <section className="home-about-main-ctn">
-            <div className="home-about-mini-ctn">
-                
+        <section className="home-team-main-ctn">
+            <div className="home-team-mini-ctn">
+                <HomeTeamTitle/>
+                <HomeTeamDesc/>
+                <br/>
+                <div className="home-team-box">
+                    <HomeTeamBoxOne/>
+                    <HomeTeamBoxTwo/>
+                </div>
             </div>
         </section>
     )
