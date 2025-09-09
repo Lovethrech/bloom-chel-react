3e54c88a52c3294440fbdcdce0fc614f9eff408d
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 16:30:52 +0100

    ...working on home component.

--- src/components/home/achieved/HomeAchievedDescBottom.jsx
+++ src/components/home/achieved/HomeAchievedDescBottom.jsx
@@ -3,7 +3,20 @@ const HomeAchievedDescBottom=()=>{
     return(
         <div className="home-achieved-desc-bottom-main-ctn">
             <div className="home-achieved-desc-bottom-mini-ctn">
-                
+                <p className="home-achieved-desc-bottom-mini-ctn-name-patients">
+                    <b className="home-achieved-desc-bottom-mini-ctn-name-patients-count">
+                        40k+
+                    </b>
+                    <br/>
+                    patients reached
+                </p>
+                <p className="home-achieved-desc-bottom-mini-ctn-name-business">
+                    <b className="home-achieved-desc-bottom-mini-ctn-name-patients-count">
+                        10+
+                    </b>
+                    <br/>
+                    business reached
+                </p>
             </div>
         </div>
     )
