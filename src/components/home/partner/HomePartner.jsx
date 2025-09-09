a481e9f70f9a33310c9b99f14e3b32728a810428
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 15:34:45 +0100

    ...working on home component.

---
+++ src/components/home/partner/HomePartner.jsx
@@ -0,0 +1,12 @@
+
+const HomePartner=()=>{
+    return(
+        <section className="home-about-main-ctn">
+            <div className="home-about-mini-ctn">
+                
+            </div>
+        </section>
+    )
+};
+
+export default HomePartner;
