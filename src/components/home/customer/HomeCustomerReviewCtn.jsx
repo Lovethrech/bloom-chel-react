c758232b3b65ce79ffda87136fabc4ef28b2d800
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 17:25:25 +0100

    ...working on home component.

--- src/components/home/customer/HomeCustomerReviewCtn.jsx
+++ src/components/home/customer/HomeCustomerReviewCtn.jsx
@@ -1,9 +1,12 @@
 import "./HomeCustomerReviewCtn.css";
+import HomeCustomerReviewImg from "./HomeCustomerReviewImg";
+import HomeCustomerReviewTextCtnDetails from "./HomeCustomerReviewTextCtnDetails";
 const HomeCustomerReviewCtn=()=>{
     return(
         <div className="home-customer-review-ctn-main-ctn">
             <div className="home-customer-review-ctn-mini-ctn">
-                
+                <HomeCustomerReviewImg/>
+                <HomeCustomerReviewTextCtnDetails/>
             </div>
         </div>
     )
