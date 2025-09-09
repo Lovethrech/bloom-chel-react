e11f7bddcb807d036a5da32faac186de2c44593d
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 14:45:13 +0100

    ...working on home component.

---
+++ src/components/home/about/HomeAboutMobileTitle.jsx
@@ -0,0 +1,14 @@
+import "./HomeAboutTitle.css";
+
+const HomeAboutTitle=()=>{
+    return(
+        <div id="mobile" className="home-about-title-main-ctn">
+            <h2 className="home-about-title-main-ctn-name">
+                The <b className="home-about-title-main-ctn-name-colored">bloom</b><br/>
+                way
+            </h2>
+        </div>
+    )
+};
+
+export default HomeAboutTitle;
