f495b09df4cae0fa19691f7312cdab808ad59d7d
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 18:00:42 +0100

    ...working on home component.

--- src/components/home/contact/HomeContactMainContentLink.jsx
+++ src/components/home/contact/HomeContactMainContentLink.jsx
@@ -5,10 +5,10 @@ const HomeContactMainContentLink=()=>{
         <div className="hhome-contact-main-content-link-main-ctn">
             <Link to="" className="link">
                 <div className="home-contact-main-content-link-mini-ctn">
-                    <p class="home-contact-main-content-link-mini-ctn-name">
+                    <p className="home-contact-main-content-link-mini-ctn-name">
                         contact us
                     </p>
-                    <i class='bx  bx-arrow-up-right-stroke'  ></i> 
+                    <i className='bx  bx-arrow-up-right-stroke'  ></i> 
                 </div>
             </Link>
         </div>
--- src/components/home/customer/HomeCustomerReviewTextCtnDetailsTitle.jsx
+++ src/components/home/customer/HomeCustomerReviewTextCtnDetailsTitle.jsx
@@ -2,7 +2,7 @@ import "./HomeCustomerReviewTextCtnDetailsTitle.css";
 const HomeCustomerReviewTextCtnDetailsTitle=()=>{
     return(
         <div className="home-customer-review-text-ctn-title-main-ctn">
-            <p class="home-customer-review-text-ctn-title-main-ctn-name">
+            <p className="home-customer-review-text-ctn-title-main-ctn-name">
                 CEO A&amp;B Pharma
             </p>
         </div>
