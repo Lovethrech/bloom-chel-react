5aecee9768f0a88ba1ba8a0d34c4208dbffd62be
Lovethrech <rachealdolapo45@gmail.com>
2025-09-09 18:29:51 +0100

    ...working on home component.

--- src/components/home/blog/HomeBlogBoxMainCtn.jsx
+++ src/components/home/blog/HomeBlogBoxMainCtn.jsx
@@ -2,10 +2,39 @@ import HomeBlogBoxOne from './HomeBlogBoxOne.jsx';
 import HomeBlogBoxTwo from './HomeBlogBoxTwo.jsx';
 import HomeBlogBoxThree from './HomeBlogBoxThree.jsx';
 import "./HomeBlogBoxMainCtn.css";
+
+const HomeBlogDetails=[
+    {
+        "id":"1",
+        "bgImgSrc":"@/assets/home-blog-img-one.svg",
+        "boxDesc":"Check out our latest news and articles about the use of technology in the medical field"
+    },
+    {
+        "id":"2",
+        "bgImgSrc":"@/assets/home-blog-img-two.svg",
+        "boxDesc":"Check out our latest news and articles about the use of technology in the medical field"
+    },
+    {
+        "id":"3",
+        "bgImgSrc":"@/assets/home-blog-img-three.svg",
+        "boxDesc":"Check out our latest news and articles about the use of technology in the medical field"
+    }
+]
 const HomeBlogBoxMainCtn=()=>{
     return(
         <div className="home-bog-box-main-ctn-main-ctn">
             <div className="home-bog-box-main-ctn-mini-ctn">
+                {HomeBlogDetails.map(HomeBlogDetail=>{
+                    return(
+                        <NavLink
+                            key={HomeBlogDetail.id}
+                            bgImgSrc={HomeBlogDetail.bgImgSrc}
+                            boxDesc={HomeBlogDetail.boxDesc}
+                            color={navLinkColor}
+                            fontSize={navLinkFontSize}
+                        />
+                    )
+                })}
                 <HomeBlogBoxOne/>
                 <HomeBlogBoxTwo/>
                 <HomeBlogBoxThree/>
