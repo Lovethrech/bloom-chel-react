451912ccef4ab260b1ef2d5095154af4dfc2dff6
Lovethrech <rachealdolapo45@gmail.com>
2025-09-10 10:40:45 +0100

    ...working on auth component.

--- src/components/auth/AuthLogo.jsx
+++ src/components/auth/AuthLogo.jsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
 import "./AuthLogo.css";
 
 const AuthLogo=()=>{
 
     return(
-        <div className="footer-desc-main-ctn">
-            
+        <div className="nav-logo-img-ctn">
+            <Link to="">
+                <img src="src/assets/bloom.svg" alt="Bloom Desktop Logo" className="nav-logo-desktop-logo"/>
+                <img src="src/assets/auth-logo-mobile.svg" alt="Bloom Mobile Logo" className="nav-logo-mobile-logo"/>
+            </Link>
         </div>
     )
 };
