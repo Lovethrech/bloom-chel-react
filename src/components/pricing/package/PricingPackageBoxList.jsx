2fe9e00b313b72dfee2194a9160c184a6cfb3bc8
Lovethrech <rachealdolapo45@gmail.com>
2025-09-10 09:36:40 +0100

    ...working on pricing component.

--- src/components/pricing/package/PricingPackageBoxList.jsx
+++ src/components/pricing/package/PricingPackageBoxList.jsx
@@ -10,11 +10,15 @@ const PricingPackageBoxList=({mark, markName})=>{
             >
                 <div 
                     className='pricing-package-box-list-mini-ctn-mark-ctn'
+
                     dangerouslySetInnerHTML={{ __html: mark }}
                 >
                 </div>
                 <div className='pricing-package-box-list-mini-ctn-mark-name-ctn'>
-                    <p className="pricing-package-box-list-mini-ctn-mark-name" style="{textDecoration:nameTextDecoration}">
+                    <p 
+                        className="pricing-package-box-list-mini-ctn-mark-name" 
+                        style="{textDecoration:nameTextDecoration}"
+                    >
                         {markName}
                     </p>
                 </div>
