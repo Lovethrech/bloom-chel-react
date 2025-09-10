66415e111aa9f2550ca2906185c8e236b9937c88
Lovethrech <rachealdolapo45@gmail.com>
2025-09-10 10:54:56 +0100

    ...working on auth component.

--- src/components/auth/AuthLoginDetailsCtn.jsx
+++ src/components/auth/AuthLoginDetailsCtn.jsx
@@ -1,10 +1,15 @@
 import "./AuthLoginDetailsCtn.css";
+import AuthLoginAndRegisterHeader from "./AuthLoginAndRegisterHeader";
+import AuthRegisterDetailsFormInput from "./AuthRegisterDetailsFormInput";
 
 const AuthLoginDetailsCtn=()=>{
     return(
         <div className="auth-register-details-ctn-main-ctn">
             <div className="auth-register-details-ctn-mini-ctn">
-            
+                <AuthLoginAndRegisterHeader/>
+                <form action="" className="auth-register-details-ctn-mini-ctn-form">
+
+                </form>
             </div>
         </div>
     )
