617935cf55b39f1ec559d65446047499dba2411f
Lovethrech <rachealdolapo45@gmail.com>
2025-09-10 10:32:59 +0100

    ...working on auth component.

---
+++ src/components/auth/AuthHeader.jsx
@@ -0,0 +1,12 @@
+import "./AuthHeader.css";
+
+const AuthHeader=()=>{
+
+    return(
+        <div className="footer-desc-main-ctn">
+            
+        </div>
+    )
+};
+
+export default AuthHeader;
