cfaf9e07d9a7a0eca154a31bfb2a8c635de5208c
Lovethrech <rachealdolapo45@gmail.com>
2025-09-10 08:41:13 +0100

    ...working on pricing component.

---
+++ src/components/pricing/hero/PricingHeroMainDescArrow.jsx
@@ -0,0 +1,18 @@
+import './PricingHeroMainDescArrow.css';
+
+const PricingHeroMainDescArrow=()=>{
+    return(
+        <div 
+            className="nav-link-main-ctn"
+        >
+            <div 
+                className="nav-link-main-ctn"
+            >
+            
+            </div>
+            
+        </div>
+    );
+};
+
+export default PricingHeroMainDescArrow;
---
+++ src/components/pricing/hero/PricingHeroMainDescTitleText.jsx
@@ -0,0 +1,18 @@
+import './PricingHeroMainDescTitleText.css';
+
+const PricingHeroMainDescTitleText=()=>{
+    return(
+        <div 
+            className="nav-link-main-ctn"
+        >
+            <div 
+                className="nav-link-main-ctn"
+            >
+            
+            </div>
+            
+        </div>
+    );
+};
+
+export default PricingHeroMainDescTitleText;
