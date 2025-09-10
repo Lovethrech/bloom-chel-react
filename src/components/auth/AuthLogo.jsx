f57af4dc4f9b707d8622c2077147c4c885a2e8b2
Lovethrech <rachealdolapo45@gmail.com>
2025-09-10 10:29:02 +0100

    ...working on auth component.

---
+++ src/components/auth/AuthLogo.jsx
@@ -0,0 +1,12 @@
+import "./AuthLogo.css";
+
+const AuthLogo=()=>{
+
+    return(
+        <div className="footer-desc-main-ctn">
+            
+        </div>
+    )
+};
+
+export default AuthLogo;
---
+++ src/components/auth/AuthRegisterBtn.jsx
@@ -0,0 +1,12 @@
+import "./AuthRegisterBtn.css";
+
+const AuthRegisterBtn=()=>{
+
+    return(
+        <div className="footer-desc-main-ctn">
+            
+        </div>
+    )
+};
+
+export default AuthRegisterBtn;
