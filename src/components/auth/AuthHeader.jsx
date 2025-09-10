5366c7ea6619381cc7839833902b4b88f8afcb69
Lovethrech <rachealdolapo45@gmail.com>
2025-09-10 10:35:12 +0100

    ...working on auth component.

--- src/components/auth/AuthHeader.jsx
+++ src/components/auth/AuthHeader.jsx
@@ -5,7 +5,9 @@ const AuthHeader=()=>{
     return(
         <div className="auth-header-main-ctn">
             <div className="auth-header-mini-ctn">
-                
+                <header className="auth-header-mini-ctn-header">
+                    
+                </header>
             </div>
         </div>
     )
