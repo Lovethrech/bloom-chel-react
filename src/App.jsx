749dbeba953e8d31516e1c6963f83685e1453b44
Lovethrech <rachealdolapo45@gmail.com>
2025-09-10 09:47:58 +0100

    ...working on pricing component.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import Home from "./components/home/Home";
 import Pricing from "./components/pricing/Pricing";
+import Auth from "./components/auth/Auth";
 
 const App=()=>{
   return(
@@ -9,6 +10,7 @@ const App=()=>{
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/pricing" element={<Pricing/>} />
+        <Route path="/auth" element={<Auth/>} />
       </Routes>
     </Router>
   )
