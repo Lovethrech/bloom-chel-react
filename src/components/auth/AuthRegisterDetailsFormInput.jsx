8e9721a7a7442e1cbff84c5b5fbda51a915b1d7b
Lovethrech <rachealdolapo45@gmail.com>
2025-09-10 11:19:47 +0100

    ...working on auth component.

--- src/components/auth/AuthRegisterDetailsFormInput.jsx
+++ src/components/auth/AuthRegisterDetailsFormInput.jsx
@@ -1,10 +1,21 @@
 import "./AuthRegisterDetailsFormInput.css";
+// :v-model="vModel" :style="{border:inputBoxBorder}" 
 
-const AuthRegisterDetailsFormInput=()=>{
 
+const AuthRegisterDetailsFormInput=({name, placeholder, nameFor, nameType})=>{
     return(
-        <div className="footer-desc-main-ctn">
-            
+        <div className="auth-register-details-form-input-main-ctn">
+            <div className="auth-register-details-form-input-mini-ctn">
+                <label for={nameFor} className="auth-register-details-form-input-mini-ctn-label">
+                    { name }
+                </label>
+                <input 
+                    id={nameFor }
+                    type={nameType }
+                    placeholder={placeholder} 
+                    className="auth-register-details-form-input-mini-ctn-input"
+                />
+            </div>
         </div>
     )
 };
