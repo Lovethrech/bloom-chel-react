a44a78da638f46d77632a667e90cd40b2a43d52b
Lovethrech <rachealdolapo45@gmail.com>
2025-09-10 08:47:11 +0100

    ...working on pricing component.

---
+++ src/components/pricing/package/PricingPackageTitle.jsx
@@ -0,0 +1,17 @@
+import './PricingPackageTitle.css';
+
+const PricingPackageTitle=()=>{
+    return(
+        <div 
+            className="nav-link-main-ctn"
+        >
+            <div 
+                className="nav-link-main-ctn"
+            >
+                
+            </div>
+        </div>
+    );
+};
+
+export default PricingPackageTitle;
