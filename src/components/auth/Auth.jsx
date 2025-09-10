cda1e484767f9b2479bea6a0eabdccaeac40af50
Lovethrech <rachealdolapo45@gmail.com>
2025-09-10 09:46:54 +0100

    ...working on pricing component.

---
+++ src/components/auth/Auth.jsx
@@ -0,0 +1,15 @@
+import "./Auth.css";
+
+import Footer from "../footer/Footer";
+
+
+const Auth=()=>{
+    return(
+        <main>
+            
+            <Footer/>
+        </main>
+    )
+};
+
+export default Auth;
