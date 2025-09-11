caca803bf4296a27ef58be0bcb4f2385ed3a834b
Lovethrech <rachealdolapo45@gmail.com>
2025-09-11 15:55:29 +0100

    ...working on pricing component.

--- src/components/auth/AuthLoginAndRegisterHeader.jsx
+++ src/components/auth/AuthLoginAndRegisterHeader.jsx
@@ -1,11 +1,15 @@
 import "./AuthLoginAndRegisterHeader.css";
 import AuthLoginAndRegisterHeaderTitle from "./AuthLoginAndRegisterHeaderTitle";
+import AuthLoginAndRegisterHeaderDesc from "./AuthLoginAndRegisterHeaderDesc";
+import AuthRegisterDetailsGoogle from "./AuthRegisterDetailsGoogle";
 const AuthLoginAndRegisterHeader=()=>{
 
     return(
         <div className="auth-login-and-register-header-main-ctn">
             <div className="auth-login-and-register-header-mini-ctn">
                 <AuthLoginAndRegisterHeaderTitle/>
+                <AuthLoginAndRegisterHeaderDesc/>
+                <AuthRegisterDetailsGoogle/>
             </div>
         </div>
     )
