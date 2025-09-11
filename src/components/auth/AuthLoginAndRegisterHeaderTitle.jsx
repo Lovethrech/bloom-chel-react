67cc601f1b1cf124249c17934b579238bd5d1dfd
Lovethrech <rachealdolapo45@gmail.com>
2025-09-11 19:19:34 +0100

    ...working on auth component.

--- src/components/auth/AuthLoginAndRegisterHeaderTitle.jsx
+++ src/components/auth/AuthLoginAndRegisterHeaderTitle.jsx
@@ -5,11 +5,7 @@ const AuthLoginAndRegisterHeaderTitle=()=>{
     return(
         <div className="auth-login-and-register-header-title-main-ctn">
             <p className="auth-login-and-register-header-title-main-ctn-name">
-                Welcome
-                to
-                <b className="auth-login-and-register-header-title-main-ctn-name-coloured">
-                    BLOOM
-                </b>
+                Welcome to <b className="auth-login-and-register-header-title-main-ctn-name-coloured"> BLOOM </b>
             </p>
         </div>
     )
