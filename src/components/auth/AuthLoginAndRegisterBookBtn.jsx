bd346c88af14d710846a052b893f92e4a9c0fca4
Lovethrech <rachealdolapo45@gmail.com>
2025-09-11 16:04:32 +0100

    ...working on auth component.

--- src/components/auth/AuthLoginAndRegisterBookBtn.jsx
+++ src/components/auth/AuthLoginAndRegisterBookBtn.jsx
@@ -1,18 +1,21 @@
+import { Link } from "react-router-dom";
 import "./AuthLoginAndRegisterBookBtn.css";
 
 const AuthLoginAndRegisterBookBtn=({email, setEmail, password, setPassword, setInputBoxBorder})=>{
     return(
-        <div 
-            className="footer-desc-main-ctn"
-            onClick={()=>{
-                if (((email==="")&&(password==="")) || ((email==="")||(password===""))){
-                    setEmail();
-                    setPassword();
-                    setInputBoxBorder("0.02vh solid red")
-                }
-            }}
-        >
-        </div>
+        <Link to="" className="link">
+            <div 
+                className="auth-login-and-register-book-btn-main-ctn"
+                onClick={()=>{
+                    if (((email==="")&&(password==="")) || ((email==="")||(password===""))){
+                        setEmail();
+                        setPassword();
+                        setInputBoxBorder("0.02vh solid red")
+                    }
+                }}
+            >
+            </div>
+        </Link>
     )
 };
 
