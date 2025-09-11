da4244154c7f994ab20ee4f4c98f9d2a503b797c
Lovethrech <rachealdolapo45@gmail.com>
2025-09-11 16:01:12 +0100

    ...working on auth component.

--- src/components/auth/AuthRegisterDetailsGoogle.jsx
+++ src/components/auth/AuthRegisterDetailsGoogle.jsx
@@ -1,10 +1,13 @@
+import {Link} from "react-router-dom";
 import "./AuthRegisterDetailsGoogle.css";
 
 const AuthRegisterDetailsGoogle=()=>{
 
     return(
         <div className="auth-register-details-google-main-ctn">
-            
+            <Link to="" className="link">
+                
+            </Link>
         </div>
     )
 };
