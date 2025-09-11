1c7d3c2f717e4d31c1bc91c6e7cb68855e081885
Lovethrech <rachealdolapo45@gmail.com>
2025-09-11 16:02:15 +0100

    ...working on auth component.

--- src/components/auth/AuthRegisterDetailsGoogle.jsx
+++ src/components/auth/AuthRegisterDetailsGoogle.jsx
@@ -6,7 +6,14 @@ const AuthRegisterDetailsGoogle=()=>{
     return(
         <div className="auth-register-details-google-main-ctn">
             <Link to="" className="link">
-                
+                <button className="auth-register-details-google-main-ctn-button">
+                    <div className="auth-register-details-google-main-ctn-button-img-ctn">
+                        <img src="@/assets/flat-color-icons_google.svg" alt=""/>
+                    </div>
+                    <p className="auth-register-details-google-main-ctn-button-name">
+                        Sign up with Google
+                    </p>
+                </button>
             </Link>
         </div>
     )
