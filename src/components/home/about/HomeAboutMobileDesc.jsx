e20459792eaf259f0b1c32c816f54dfcd06f84ff
Lovethrech <rachealdolapo45@gmail.com>
2025-09-14 01:37:40 +0100

    ...working on all component.

--- src/components/home/about/HomeAboutMobileDesc.jsx
+++ src/components/home/about/HomeAboutMobileDesc.jsx
@@ -2,8 +2,13 @@ import "./HomeAboutMobileDesc.css";
 
 const HomeAboutMobileDesc=()=>{
     return(
-        <div id="mobile" className="home-about-desc-main-ctn">
-            <p className="home-about-desc-main-ctn-name">
+        <div 
+            id="mobile" 
+            className="home-about-desc-main-ctn"
+        >
+            <p 
+                className="home-about-desc-main-ctn-name"
+            >
                 Access all important documents in one place, Run 
                 your business from anywhere in the world, Manage 
                 all income and expenditure in one place, Connect 
