dac23335ed976028681f8149780232f21852ee40
Lovethrech <rachealdolapo45@gmail.com>
2025-09-14 01:39:20 +0100

    ...working on all component.

--- src/components/home/about/HomeAboutMobileTryFreeBtn.jsx
+++ src/components/home/about/HomeAboutMobileTryFreeBtn.jsx
@@ -3,11 +3,21 @@ import "./HomeAboutMobileTryFreeBtn.css";
 
 const HomeAboutMobileTryFreeBtn=({tryFreeBtnBgColor, tryFreeBtnColor})=>{
     return(
-        <div id="mobile" className="try-for-free-btn-main-ctn">
-            <Link to="/auth">
+        <div 
+            id="mobile" 
+            className="try-for-free-btn-main-ctn"
+        >
+            <Link 
+                to="/auth"
+            >
                 <button 
                     className="try-for-free-btn"
-                    style={{background:tryFreeBtnBgColor, color:tryFreeBtnColor}}
+                    style={
+                        {
+                            background:tryFreeBtnBgColor, 
+                            color:tryFreeBtnColor
+                        }
+                    }
                 >
                     Try for free
                 </button>
