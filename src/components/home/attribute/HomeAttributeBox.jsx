af3ab6d52bbb4bb5dae59839d07d032b9b6fcbfd
Lovethrech <rachealdolapo45@gmail.com>
2025-09-14 01:46:25 +0100

    ...working on all component.

--- src/components/home/attribute/HomeAttributeBox.jsx
+++ src/components/home/attribute/HomeAttributeBox.jsx
@@ -4,8 +4,12 @@ import HomeAttributeBoxImg from "./HomeAttributeBoxImg";
 import HomeAttributeBoxTitle from "./HomeAttributeBoxTitle";
 const HomeAttributeBox=({name, imgSrc, description})=>{
     return(
-        <div className="home-attribute-box-main-ctn">
-            <div className="home-attribute-box-mini-ctn">
+        <div 
+            className="home-attribute-box-main-ctn"
+        >
+            <div 
+                className="home-attribute-box-mini-ctn"
+            >
                 <HomeAttributeBoxImg
                     imgSrc={imgSrc}
                 />
