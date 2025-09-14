9a05e23904eac3df8ea43db93f50cd87774777db
Lovethrech <rachealdolapo45@gmail.com>
2025-09-14 01:32:43 +0100

    ...working on all component.

--- src/components/home/about/HomeAboutDesktopTryFreeBtn.jsx
+++ src/components/home/about/HomeAboutDesktopTryFreeBtn.jsx
@@ -3,8 +3,13 @@ import "./HomeAboutDesktopTryFreeBtn.css";
 
 const HomeAboutDesktopTryFreeBtn=({tryFreeBtnBgColor, tryFreeBtnColor})=>{
     return(
-        <div id="desktop" className="try-for-free-btn-main-ctn">
-            <Link to="/auth">
+        <div 
+            id="desktop" 
+            className="try-for-free-btn-main-ctn"
+        >
+            <Link 
+                to="/auth"
+            >
                 <button 
                     className="try-for-free-btn"
                     style={{background:tryFreeBtnBgColor, color:tryFreeBtnColor}}
