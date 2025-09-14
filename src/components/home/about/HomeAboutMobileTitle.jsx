4183be890875eb12c54c8e72298db4946f84ba59
Lovethrech <rachealdolapo45@gmail.com>
2025-09-14 01:38:19 +0100

    ...working on all component.

--- src/components/home/about/HomeAboutMobileTitle.jsx
+++ src/components/home/about/HomeAboutMobileTitle.jsx
@@ -2,8 +2,13 @@ import "./HomeAboutMobileTitle.css";
 
 const HomeAboutMobileTitle=()=>{
     return(
-        <div id="mobile" className="home-about-title-main-ctn">
-            <h2 className="home-about-title-main-ctn-name">
+        <div 
+            id="mobile" 
+            className="home-about-title-main-ctn"
+        >
+            <h2 
+                className="home-about-title-main-ctn-name"
+            >
                 The <b className="home-about-title-main-ctn-name-colored">bloom</b><br/>
                 way
             </h2>
