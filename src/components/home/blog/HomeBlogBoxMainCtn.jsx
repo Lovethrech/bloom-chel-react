6295ee2dff3a9018fba9cd97a0724851869ae7b2
Lovethrech <rachealdolapo45@gmail.com>
2025-09-14 04:33:17 +0100

    ...working on all component.

--- src/components/home/blog/HomeBlogBoxMainCtn.jsx
+++ src/components/home/blog/HomeBlogBoxMainCtn.jsx
@@ -2,23 +2,6 @@ import HomeBlogBox from './HomeBlogBox.jsx';
 import HomeBlogDetails from  "/src/data/home-blog-details.json";
 import "./HomeBlogBoxMainCtn.css";
 
-const HomeBlogDetails=[
-    {
-        "id":"1",
-        "bgImgSrc":"<img src='src/assets/home-blog-img-one.svg' alt='Blog Image'/>",
-        "boxDesc":"Check out our latest news and articles about the use of technology in the medical field"
-    },
-    {
-        "id":"2",
-        "bgImgSrc":"<img src='src/assets/home-blog-img-two.svg' alt='Blog Image'/>",
-        "boxDesc":"Check out our latest news and articles about the use of technology in the medical field"
-    },
-    {
-        "id":"3",
-        "bgImgSrc":"<img src='src/assets/home-blog-img-three.svg' alt='Blog Image'/>",
-        "boxDesc":"Check out our latest news and articles about the use of technology in the medical field"
-    }
-]
 const HomeBlogBoxMainCtn=()=>{
     return(
         <div className="home-bog-box-main-ctn-main-ctn">
