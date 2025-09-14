ed821e33bdee5f3109cf259c633c465750a2d2f6
Lovethrech <rachealdolapo45@gmail.com>
2025-09-14 01:32:06 +0100

    ...working on all component.

--- src/components/home/about/HomeAboutDesc.jsx
+++ src/components/home/about/HomeAboutDesc.jsx
@@ -2,8 +2,12 @@ import "./HomeAboutDesc.css";
 
 const HomeAboutDesc=()=>{
     return(
-        <div className="home-about-desc-main-ctn">
-            <p className="home-about-desc-main-ctn-name">
+        <div 
+            className="home-about-desc-main-ctn"
+        >
+            <p 
+                className="home-about-desc-main-ctn-name"
+            >
                 Access all important documents in one place, Run 
                 your business from anywhere in the world, Manage 
                 all income and expenditure in one place, Connect 
