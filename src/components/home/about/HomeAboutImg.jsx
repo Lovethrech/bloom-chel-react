9fdd2f2cb079b52ffae08452e1dc2fc66d92db1d
Lovethrech <rachealdolapo45@gmail.com>
2025-09-14 01:37:08 +0100

    ...working on all component.

--- src/components/home/about/HomeAboutImg.jsx
+++ src/components/home/about/HomeAboutImg.jsx
@@ -2,8 +2,13 @@ import "./HomeAboutImg.css";
 
 const HomeAboutImg=()=>{
     return(
-        <div className="home-about-img-main-ctn">
-            <img src="src/assets/home-about-img.svg" alt="Home About Section Image"/>
+        <div 
+            className="home-about-img-main-ctn"
+        >
+            <img 
+                src="src/assets/home-about-img.svg"
+                alt="Home About Section Image"
+            />
         </div>
     )
 };
