ebcbb00b03f86dc7d4432f4f06e5b9b8958030d3
Lovethrech <rachealdolapo45@gmail.com>
2025-09-14 01:58:07 +0100

    ...working on all component.

--- src/components/home/attribute/HomeAttributeBoxDesc.jsx
+++ src/components/home/attribute/HomeAttributeBoxDesc.jsx
@@ -1,8 +1,12 @@
 import "./HomeAttributeBoxDesc.css";
 const HomeAttributeBoxDesc=({description})=>{
     return(
-        <div className="home-attribute-box-desc-main-ctn">
-            <p className="home-attribute-box-desc-main-ctn-name">
+        <div 
+            className="home-attribute-box-desc-main-ctn"
+        >
+            <p 
+                className="home-attribute-box-desc-main-ctn-name"
+            >
                 {description}
             </p>
         </div>
