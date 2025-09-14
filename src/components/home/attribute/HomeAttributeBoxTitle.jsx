5e3d90dd35688848ad5bab6e47d126855ca85838
Lovethrech <rachealdolapo45@gmail.com>
2025-09-14 01:59:06 +0100

    ...working on all component.

--- src/components/home/attribute/HomeAttributeBoxTitle.jsx
+++ src/components/home/attribute/HomeAttributeBoxTitle.jsx
@@ -1,8 +1,12 @@
 import "./HomeAttributeBoxTitle.css";
 const HomeAttributeBoxTitle=({name})=>{
     return(
-        <div className="home-attribute-box-title-main-ctn">
-            <p className="home-attribute-box-title-main-ctn-name">
+        <div 
+            className="home-attribute-box-title-main-ctn"
+        >
+            <p 
+                className="home-attribute-box-title-main-ctn-name"
+            >
                 {name}
             </p>
         </div>
