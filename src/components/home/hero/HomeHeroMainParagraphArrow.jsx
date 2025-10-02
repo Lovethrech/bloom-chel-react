3fc2a586b3494e732d492bc78c8d67e203e3fc91
Lovethrech <rachealdolapo45@gmail.com>
2025-10-02 20:14:05 +0100

    ...working on all component.

--- src/components/home/hero/HomeHeroMainParagraphArrow.jsx
+++ src/components/home/hero/HomeHeroMainParagraphArrow.jsx
@@ -11,7 +11,7 @@ const HomeHeroMainParagraphArrow=()=>{
             onClick={navToAboutComponent}
         >
             <img 
-                src="src/a"
+                src="src/"
                 alt="Hero ArrowDown"
             /> 
         </div>
