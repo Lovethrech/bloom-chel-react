f52cd94bac083a7b84f8d9237a28c4349b61e323
Lovethrech <rachealdolapo45@gmail.com>
2025-10-03 20:15:54 +0100

    ...working on all component.

---
+++ src/components/home/hero/HomeHeroMainParagraphArrow.jsx
@@ -0,0 +1,21 @@
+const navToAboutComponent=()=>{
+    const target=document.getElementById('about');
+    if (!target) return;
+    target.scrollIntoView({ behavior: 'smooth' });
+}
+
+const HomeHeroMainParagraphArrow=()=>{
+    return(
+        <div 
+            className="home-hero-main-desc-paragraph-arrow-main-ctn" 
+            onClick={navToAboutComponent}
+        >
+            <img 
+                src=""
+                alt="Hero ArrowDown"
+            /> 
+        </div>
+    )
+};
+
+export default HomeHeroMainParagraphArrow;
