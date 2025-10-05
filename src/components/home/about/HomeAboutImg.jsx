8b8e0799e8b0f9f06b7891aca01556f2c8f9d781
Lovethrech <rachealdolapo45@gmail.com>
2025-10-05 20:19:17 +0100

    ...working on all component.

--- src/components/home/about/HomeAboutImg.jsx
+++ src/components/home/about/HomeAboutImg.jsx
@@ -6,7 +6,7 @@ const HomeAboutImg=()=>{
             className="home-about-img-main-ctn"
         >
             <img 
-                src="src/assets/home-about-i"
+                src="src/assets/home-about-"
                 alt="Home About Section Image"
             />
         </div>
