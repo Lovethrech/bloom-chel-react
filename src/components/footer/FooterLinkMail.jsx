511eb2f7129636b8055c5b66d2cd16c91fb40f1f
Lovethrech <rachealdolapo45@gmail.com>
2025-10-07 21:21:01 +0100

    ...working on all component.

--- src/components/footer/FooterLinkMail.jsx
+++ src/components/footer/FooterLinkMail.jsx
@@ -10,7 +10,7 @@ const FooterLinkMail=()=>{
                     Send us an email
                 </p>
                 <div className="footer-link-mail-main-content-link-mini-ctn-arrow-ctn">
-                    <img src="src/assets/footer-mail.svg" alt="Footer link Mail Icon"/>
+                    <img src="/footer-mail.svg" alt="Footer link Mail Icon"/>
                 </div>
             </Link>
         </div>
