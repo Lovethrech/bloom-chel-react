775af8c6c134c9ff5b7f41dd6984e46f146dab99
Lovethrech <rachealdolapo45@gmail.com>
2025-10-07 22:01:14 +0100

    ...working on all component.

--- src/components/footer/FooterLinkMail.jsx
+++ src/components/footer/FooterLinkMail.jsx
@@ -6,12 +6,15 @@ const FooterLinkMail=()=>{
     return(
         <div className="footer-link-mail-main-content-link-main-ctn">
             <Link to="" className="link">
+            <div class="footer-link-mail-main-content-link-mini-ctn">   
                 <p className="footer-link-mail-main-content-link-mini-ctn-name">
                     Send us an email
                 </p>
                 <div className="footer-link-mail-main-content-link-mini-ctn-arrow-ctn">
                     <img src="/footer-mail.svg" alt="Footer link Mail Icon"/>
                 </div>
+
+            </div>
             </Link>
         </div>
     )
